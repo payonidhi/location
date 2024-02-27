f3889f3ea5680815acf84ce910b52fd85a452191
payonidhi <payonidhi.20cse383@rtu.ac.in>
2024-02-27 17:09:07 +0530

    demolocation

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import GetCurrentAddress from './GetCurrentAddress/GetCurrentAddress';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>This is my current location</h1>
+     <GetCurrentAddress/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/GetCurrentAddress/GetCurrentAddress.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+
+function GetCurrentAddress() {
+    const [add, setAdd] = useState('')
+    const [latitude, setLatitude] = useState('');
+    const [longitude, setLongitude] = useState('');
+    // `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(pos => {
+            const { latitude, longitude } = pos.coords;
+            setLatitude(latitude);
+            setLongitude(longitude);
+            console.log(latitude, longitude)
+            const url = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`;
+            fetch(url).then(res => res.json()).then(data => setAdd(data.address))
+        })
+    }, [])
+    console.log(add, "sfsfh")
+    return (
+        <>
+            <p>road : {add.road}</p>
+            
+            <p>city : {add.city}</p>
+            <p>State :{add.state}</p>
+            <p>district :{add.state_district}</p>
+            <p>country :{add.country}</p>
+            <p>Latitude :{latitude}</p>
+            <p>Longitude :{longitude}</p>
+        </>
+    )
+}
+
+export default GetCurrentAddress;
